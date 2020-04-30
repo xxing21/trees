1fcb8e5b53b26e1e6cc053a2b06278d4c78457c9
xxing21 <xxing21@cmc.edu>
2020-04-30 16:03:02 -0700

    remove not fix

--- Trees/Heap.py
+++ Trees/Heap.py
@@ -1,15 +1,6 @@
-'''
-'''
-
 from Trees.BinaryTree import BinaryTree, Node
 
-class Heap():
-    '''
-    FIXME:
-    Heap is currently not a subclass of BinaryTree.
-    You should make the necessary changes in the class declaration line above 
-    and in the constructor below.
-    '''
+class Heap(BinaryTree):
 
     def __init__(self, xs=None):
         '''
@@ -17,7 +8,9 @@ class Heap():
         If xs is a list (i.e. xs is not None),
         then each element of xs needs to be inserted into the Heap.
         '''
-
+        super().__init__()
+        if xs:
+            self.insert_list(xs)
 
     def __repr__(self):
         '''
@@ -27,7 +20,6 @@ class Heap():
         Recall that the __repr__ function should return a string that can be used to recreate a valid instance of the class.
         Thus, if you create a variable using the command Heap([1,2,3])
         it's __repr__ will return "Heap([1,2,3])"
-
         For the Heap, type(self).__name__ will be the string "Heap",
         but for the AVLTree, this expression will be "AVLTree".
         Using this expression ensures that all subclasses of Heap will have a correct implementation of __repr__,
@@ -48,14 +40,31 @@ class Heap():
             return Heap._is_heap_satisfied(self.root)
         return True
 
+
     @staticmethod
     def _is_heap_satisfied(node):
         '''
         FIXME:
         Implement this method.
-        The lecture videos have the exact code you need,
-        except that their method is an instance method when it should have been a static method.
         '''
+        is_left_satisfied = True
+        is_right_satisfied = True
+
+        if node is None:
+            return True
+        if node.left:
+            if node.value <= node.left.value:
+                return False
+            else:
+                is_left_satisfied = Heap._is_heap_satisfied(node.left)
+
+        if node.right:
+            if node.value <= node.right.value:
+                return False
+            else:
+                is_right_satisfied = Heap._is_heap_satisfied(node.right)
+
+        return is_right_satisfied and is_left_satisfied
 
 
     def insert(self, value):
@@ -72,40 +81,109 @@ class Heap():
     @staticmethod
     def _insert(value, node):
         '''
-        FIXME:
-        Implement this function.
+        use binary representation of total nodes to determine which subtree to go
         '''
+        node.descendents += 1
+        binary = "{0:b}".format(node.descendents)
+        if binary[1] == '0':
+            if node.left is None:
+                node.left = Node(value)
+                node.left.descendents = 1
+            else:
+                Heap._insert(value,node.left)
+            if node.value > node.left.value:
+                node.value, node.left.value = node.left.value, node.value
+        elif binary[1] == '1':
+            if node.right is None:
+                node.right = Node(value)
+                node.right.descendents = 1
+            else:
+                Heap._insert(value,node.right)
+            if node.value > node.right.value:
+                node.value, node.right.value = node.right.value, node.value
 
 
     def insert_list(self, xs):
         '''
         Given a list xs, insert each element of xs into self.
-
-        FIXME:
-        Implement this function.
         '''
+        for x in xs:
+            self.insert(x)
 
 
     def find_smallest(self):
         '''
         Returns the smallest value in the tree.
-
-        FIXME:
-        Implement this function.
-        This function is not implemented in the lecture notes,
-        but if you understand the structure of a Heap it should be easy to implement.
-
-        HINT:
-        Create a recursive staticmethod helper function,
-        similar to how the insert and find functions have recursive helpers.
         '''
+        if self.root:
+            return Heap._find_smallest(self.root)
+
+
+    @staticmethod
+    def _find_smallest(node):
+        return node.value
 
 
     def remove_min(self):
         '''
-        Removes the minimum value from the Heap. 
+        Removes the minimum value from the Heap.
         If the heap is empty, it does nothing.
-
-        FIXME:
-        Implement this function.
         '''
+        self.root.value = Heap._remove_last_element(self.root)
+        if not Heap._is_heap_satisfied(self.root):
+            Heap._swap(self.root)
+
+
+    @staticmethod
+    def _remove_last_element(node):
+        binary = "{0:b}".format(node.descendents)
+        if len(binary) == 2:
+            if binary[1] == '1':
+                tmp = node.right
+                node.right = None
+            elif binary[1] == '0':
+                tmp = node.left
+                node.left = None
+            return tmp.value
+        else:
+            if binary[1] == '0':
+                return Heap._remove_last_element(node.left)
+            elif binary[1] == '1':
+                return Heap._remove_last_element(node.right)
+        node.descendents -= 1
+
+
+    @staticmethod
+    def _swap(node):
+        if node.left is None and node.right is None:
+            return node
+        elif node.right is None:
+            node.value, node.left.value = node.left.value, node.value
+            return node
+        else:
+            if node.left.value < node.right.value:
+                node.value, node.left.value = node.left.value, node.value
+                if not Heap._is_heap_satisfied(node.left):
+                    Heap._swap(node.left)
+            else:
+                node.value, node.right.value = node.right.value, node.value
+                if not Heap._is_heap_satisfied(node.right):
+                    Heap._swap(node.right)
+
+'''
+heap = Heap()
+heap.insert(1)
+heap.insert(2)
+heap.insert(3)
+heap.insert(4)
+heap.insert(5)
+heap.insert(6)
+heap.insert(7)
+heap.insert(8)
+heap.insert(9)
+heap.insert(10)
+heap.insert(11)
+print(heap)
+heap.remove_min()
+print(heap)
+'''
